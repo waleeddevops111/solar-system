564d124d3ef95f55168129c9919e0c80dcb74a0c
waleeddevops111 <devopswaleed@gmail.com>
2025-05-03 02:45:58 +0500

    updated app.js

--- app.js
+++ app.js
@@ -79,9 +79,9 @@ app.get('/ready',   function(req, res) {
     });
 })
 
-app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
-})
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Server successfully running on port - " + 3000);
+});
 
 
 module.exports = app;
